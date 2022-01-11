8bb7251f6b2fef64e8018089248090f0d10081aa
Mary Kasparian <kasparian.mary@gmail.com>
2022-01-10 18:00:03 -0600

    Fix mortgage cacluator

--- lesson_2/mortgage_calculator.rb
+++ lesson_2/mortgage_calculator.rb
@@ -3,7 +3,7 @@ require 'yaml'
 MESSAGES = YAML.load_file('mortgage_messages.yml')
 
 # ---- METHODS ------
-def messages(message, lang='en')
+def messages(message, lang=LANGUAGE)
   MESSAGES[lang][message]
 end
 
@@ -12,28 +12,19 @@ def prompt(message)
 end
 
 # Check for valid number
-def integer?(input)
-  input.to_i.to_s == input
+def number?(input)
+  input.to_i.to_s == input || input.to_f.to_s == input
 end
 
-def float?(input)
-  input.to_f.to_s == input
-end
-
-def valid_number?(input)
-  integer?(input) || float?(input)
-end
-
-# validate if input is a positive number
 def positive_number?(input)
   input.to_i >= 0 || input.to_f >= 0
 end
 
-def generate_apr(interest)
+def percentage(interest)
   interest.to_f / 100
 end
 
-def calc_payment(amount, monthly_interest, months)
+def payment(amount, monthly_interest, months)
   amount.to_f * (monthly_interest / (1 - (1 + monthly_interest)**(-months)))
 end
 
@@ -56,7 +47,7 @@ def get_loan_amount
     prompt(messages('loan_amount'))
     loan_amount = gets.chomp
 
-    if valid_number?(loan_amount) && positive_number?(loan_amount)
+    if number?(loan_amount) && positive_number?(loan_amount)
       return loan_amount
     else
       prompt(messages('valid_number'))
@@ -70,7 +61,7 @@ def get_loan_duration
     prompt(messages('loan_duration'))
     years = gets.chomp
 
-    if valid_number?(years) && positive_number?(years)
+    if number?(years) && positive_number?(years)
       return years
     else
       prompt(messages('valid_number'))
@@ -85,7 +76,7 @@ def get_interest_rate
     prompt(messages('rate_example'))
     interest_rate = gets.chomp
 
-    if valid_number?(interest_rate) && positive_number?(interest_rate)
+    if number?(interest_rate) && positive_number?(interest_rate)
       return interest_rate
     else
       prompt(messages('valid_number'))
@@ -94,7 +85,7 @@ def get_interest_rate
 end
 
 def display_payment(amount, monthly_interest, months)
-  monthly_payment = calc_payment(amount, monthly_interest, months)
+  monthly_payment = payment(amount, monthly_interest, months)
   puts "Your monthly payment is $#{format('%.2f', monthly_payment)}"
 end
 
@@ -109,15 +100,11 @@ get_name()
 # ------ MAIN PROGRAM ------
 loop do
   loan_amount = get_loan_amount()
-  years = get_loan_duration()
-  interest_rate = get_interest_rate()
-
+  months = get_loan_duration().to_i() * 12
   # calculate annual interest rate (APR)
-  annual_interest_rate = generate_apr(interest_rate)
+  annual_interest_rate = percentage(get_interest_rate())
   # calculate the monthly interest rate using the APR
   monthly_interest_rate = annual_interest_rate / 12
-  # calculate the loan duration in months
-  months = years.to_i * 12
 
   # method that calculates and displays monthly payment
   display_payment(loan_amount, monthly_interest_rate, months)
@@ -127,5 +114,6 @@ loop do
   break unless replay?(answer)
 end
 
+# ------ CLOSING MESSAGE ------
 prompt(messages('closing_message'))
 prompt("Goodbye!")
